6bc37a1e43014736eb3bb23ff613ebc341336983
Thomas Pedersen <s103665@student.dtu.dk>
2014-10-26 12:01:55 +0100

    Solver file added. No real implementation yet.

---
+++ Hardproblems/Solver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardproblems
+{
+    public class Solver
+    {
+        public List<string> Solve(Dekoder instance)
+        {
+            solve(instance, new Dictionary<char, string>());
+
+            //TODO need implementation.
+            throw new NotImplementedException();
+        }
+
+        private void solve(Dekoder instance, Dictionary<char, string> variablesAssigned)
+        {
+            foreach (char variable in instance.GammaAlphabet)
+            {
+                if (!variablesAssigned.ContainsKey(variable))
+                {
+                    foreach (string RItem in instance.R[variable])
+                    {
+                        variablesAssigned[variable] = RItem;
+                    }
+                }
+            }
+        }
+
+        private bool IsValidSolution(Dekoder instance, Dictionary<char, string> variablesAssigned)
+        {
+            //TODO implement
+            return false;
+        }
+    }
+}
